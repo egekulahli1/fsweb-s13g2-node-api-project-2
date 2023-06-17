aa3a6c2e0c69d80f59a7b753e2ff1ff42abed54d
egekulahli <egekulahli@gmail.com>
2023-06-17 05:36:53 +0300

    initial changes

--- api/server.js
+++ api/server.js
@@ -1,3 +1,13 @@
 // server için gerekli olanları burada ayarlayın
+const express = require('express');
+const server = express();
+
+server.use(express.json()); // json body parser middleware
+
+const postRouter = require('./posts/posts-router.js');
+
+server.use('/api/posts', postRouter);
 
 // posts router'ını buraya require edin ve bağlayın
+
+module.exports = server;
